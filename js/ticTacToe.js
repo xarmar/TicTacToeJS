b481e236dbd5fea1c6b83f5726c3660159a36d72
Xavier Marques <xavierpmarques@gmail.com>
2021-06-05 23:15:11 +0100

    Organize code with Module Pattern - use 3 different Immediately Invoked Function Expressions

--- js/ticTacToe.js
+++ js/ticTacToe.js
@@ -1,17 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
     var gameBoardDiv = document.querySelector("#gameBoard");
     var restartButton = document.querySelector("#restartButton");
-    // Define playerFactory
-    var playerFactory = function (name, number, symbol, ownTurn) {
-        return { name: name, number: number, symbol: symbol, ownTurn: ownTurn };
-    };
-    // Create two new players. playerOne goes first
-    var playerOne = playerFactory("Alpha", 1, "X", true);
-    var playerTwo = playerFactory("Omega", 2, "O", false);
     var gameHasEnded;
-    // Game Module to start game, reset game, toggle player turns, add marks, etc.
-    var ticTacToeGame = (function () {
+    // playerModule - playerFactory, toggleTurns
+    var playerModule = (function () {
+        var playerFactory = function (name, number, symbol, ownTurn) {
+            return { name: name, number: number, symbol: symbol, ownTurn: ownTurn };
+        };
+        // toggles who's turn it is: Player1 or Player 2.
+        var toggleTurns = function () {
+            playerOne.ownTurn ? playerOne.ownTurn = false : playerOne.ownTurn = true;
+            playerTwo.ownTurn ? playerTwo.ownTurn = false : playerTwo.ownTurn = true;
+        };
+        return {
+            playerFactory: playerFactory,
+            toggleTurns: toggleTurns
+        };
+    })();
+    // gameBoardModule - sets gameboard, starts game(populates DOM), adds player marks, etc.
+    var gameBoardModule = (function () {
         var gameBoard = ["", "", "", "", "", "", "", "", ""];
+        // gets current gameBoard
+        var getGameBoard = function () {
+            return gameBoard;
+        };
+        // sets gameBoard with a specific symbol
+        var setGameBoard = function (index, symbol) {
+            gameBoard[index] = symbol;
+        };
         // populate gameBoard with 9 empty divs.
         var initGameBoard = function () {
             for (var index in gameBoard) {
@@ -23,24 +39,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 gameBoardDiv.appendChild(div);
             }
         };
-        // remove the child elements of gameBoard from DOM, and run initGameBoard.
-        var resetGame = function () {
-            while (gameBoardDiv.firstChild) {
-                gameBoardDiv.removeChild(gameBoardDiv.firstChild);
-            }
-            for (var index = 0; index < gameBoard.length; index++) {
-                gameBoard[index] = "";
-            }
-            gameHasEnded = false;
-            playerOne.ownTurn = true;
-            playerTwo.ownTurn = false;
-            initGameBoard();
-        };
-        // toggles who's turn it is: Player1 or Player 2.
-        var toggleTurns = function () {
-            playerOne.ownTurn ? playerOne.ownTurn = false : playerOne.ownTurn = true;
-            playerTwo.ownTurn ? playerTwo.ownTurn = false : playerTwo.ownTurn = true;
-        };
         // Adds player's symbol to clicked div.
         var addMark = function (e) {
             if (!gameHasEnded) {
@@ -49,27 +47,42 @@ document.addEventListener("DOMContentLoaded", function () {
                     if (playerOne.ownTurn && !playerTwo.ownTurn) {
                         e.target.textContent = playerOne.symbol;
                         gameBoard[e.target.id] = playerOne.symbol;
-                        toggleTurns();
-                        checkForWinner(gameBoard);
+                        playerModule.toggleTurns();
+                        gameModule.lookForWinner();
                     }
                     else if (!playerOne.ownTurn && playerTwo.ownTurn) {
                         e.target.textContent = playerTwo.symbol;
                         gameBoard[e.target.id] = playerTwo.symbol;
-                        toggleTurns();
-                        checkForWinner(gameBoard);
+                        playerModule.toggleTurns();
+                        gameModule.lookForWinner();
                     }
                 }
             }
         };
-        restartButton.addEventListener("click", resetGame);
         return {
+            gameBoard: gameBoard,
             initGameBoard: initGameBoard,
-            resetGame: resetGame,
-            gameBoard: gameBoard
+            getGameBoard: getGameBoard,
+            setGameBoard: setGameBoard
         };
     })();
-    // Winner and Draw Module Checker
-    var checkForWinner = (function (gameBoard) {
+    // gameModule - tracks winners, draws and resets game.
+    var gameModule = (function () {
+        var gameBoard = gameBoardModule.getGameBoard();
+        // resetGame: remove the child elements of gameBoard from DOM, and run initGameBoard.
+        var resetGame = function () {
+            while (gameBoardDiv.firstChild) {
+                gameBoardDiv.removeChild(gameBoardDiv.firstChild);
+            }
+            for (var index = 0; index < gameBoard.length; index++) {
+                gameBoardModule.setGameBoard(index, "");
+            }
+            gameHasEnded = false;
+            playerOne.ownTurn = true;
+            playerTwo.ownTurn = false;
+            gameBoardModule.initGameBoard();
+        };
+        // winner gameController
         var winningCombinations = [
             [0, 1, 2],
             [3, 4, 5],
@@ -80,23 +93,35 @@ document.addEventListener("DOMContentLoaded", function () {
             [0, 4, 8],
             [2, 4, 6]
         ];
-        if (!gameHasEnded) {
-            winningCombinations.forEach(function (winPossibility) {
-                var symbols = [];
-                winPossibility.forEach(function (index) {
-                    symbols.push(gameBoard[index]);
+        var lookForWinner = function () {
+            if (!gameHasEnded) {
+                winningCombinations.forEach(function (winPossibility) {
+                    var symbols = [];
+                    winPossibility.forEach(function (index) {
+                        symbols.push(gameBoard[index]);
+                    });
+                    if (symbols.toString() === "X,X,X") {
+                        alert("The Winner is " + playerOne.name);
+                        gameHasEnded = true;
+                    }
+                    else if (symbols.toString() === "O,O,O") {
+                        alert("The Winner is " + playerTwo.name);
+                        gameHasEnded = true;
+                    }
                 });
-                if (symbols.toString() === "X,X,X") {
-                    alert("The Winner is " + playerOne.name);
-                    gameHasEnded = true;
-                }
-                else if (symbols.toString() === "O,O,O") {
-                    alert("The Winner is " + playerTwo.name);
-                    gameHasEnded = true;
-                }
-            });
-        }
-    });
+            }
+            ;
+        };
+        return {
+            resetGame: resetGame,
+            lookForWinner: lookForWinner
+        };
+    })();
+    // Create two new players. playerOne goes first
+    var playerOne = playerModule.playerFactory("Alpha", 1, "X", true);
+    var playerTwo = playerModule.playerFactory("Omega", 2, "O", false);
     // Starts the Game By Populating the Gameboard
-    ticTacToeGame.initGameBoard();
+    gameBoardModule.initGameBoard();
+    // Listen for clicks to restart Game
+    restartButton.addEventListener("click", gameModule.resetGame);
 });
